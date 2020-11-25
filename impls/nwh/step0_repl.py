e7df3ca671b8d9c7c36b179a8b1532e45086068b
Nick Henderson <nick.henderson@gmail.com>
2020-11-25 10:46:19 -0800

    step 0 pass

---
+++ impls/nwh/step0_repl.py
@@ -0,0 +1,24 @@
+"""mal step0"""
+
+def READ(arg):
+    return arg
+
+def EVAL(arg):
+    return arg
+
+def PRINT(arg):
+    return arg
+
+def rep(arg):
+    return PRINT(EVAL(READ(arg)))
+
+def main():
+    while True:
+        try:
+            arg = input("user> ")
+            print(rep(arg))
+        except EOFError:
+            break
+
+if __name__ == "__main__":
+    main()
